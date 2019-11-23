a14f9733f259da71a7a8434c9dff28f323696656
Karmen Liang <jl24@williams.edu>
2019-11-23 15:26:00 -0500

    step 8 checkpoint

--- src/pathtrace.cpp
+++ src/pathtrace.cpp
@@ -1,38 +1,74 @@
 #include <iostream>
+#include <float.h>
 
-#include "ray.h"
+#include "surface_list.h"
+#include "sphere.h"
+#include "camera.h"
 
-// CUDA libraries
-// #include <cuda_runtime.h>
-// #include <helper.cuda.h>
+#ifndef RANDOMH
+#define RANDOMH
 
-// Simple gradient background
-vec3 color(const ray& r) {
-    vec3 unit_dir = unit_vector(r.direction());
-    float t = 0.5 * (unit_dir.y() + 1.0);
+#include <cstdlib>
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+#endif
+
+// Pick random pt in a unit radius centered at origin
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(random_double(), random_double(), random_double()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
+// Simple gradient background scene
+vec3 color(const ray& r, surface *world) {
+  hit_record rec;
+    if (world->hit(r, 0.001, FLT_MAX, rec)) {
+      vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+      return 0.5 * color(ray(rec.p, target - rec.p), world);
+    }
     
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    else {
+      vec3 unit_dir = unit_vector(r.direction());
+      float t = 0.5*(unit_dir.y() + 1.0);
+    
+      return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
 }
 
 // Slowly building up pieces of the path tracer
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100; // number of samples
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    // Camera 'eye'
-    vec3 origin(0.0, 0.0, 0.0);
+    camera cam;
 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
+    surface *list[2];
+    list[0] = new sphere(vec3(0,0,-1), 0.5);
+    list[1] = new sphere(vec3(0,-100.5,-1), 100);
+    surface *world = new surface_list(list,2);
 
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
+            vec3 col(0, 0, 0);
+
+	    // Anti-aliasing
+	    for (int s = 0; s < ns; s++) {
+                float u = float(i + random_double()) / float(nx);
+                float v = float(j + random_double()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world);
+            }
+	    
+            col /= float(ns);
+	    col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
 
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
