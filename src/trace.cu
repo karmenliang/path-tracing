766e053e5f18d2b391ad5646857d996467cf2d0d
Karmen Liang <jl24@williams.edu>
2019-12-02 18:02:14 -0500

    Add vec3 header

--- src/trace.cu
+++ src/trace.cu
@@ -8,21 +8,25 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 
+#include "vec3.h"
+
 /*
  * CUDA kernel function
  */
-__global__ void render(float *fb, int max_x, int max_y) {
+__global__ void render(vec3 *fb, int max_x, int max_y) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
 
   // Only run on pixels within the image
   if((i >= max_x) || (j >= max_y)) return;
 
-  int pixel_index = j*max_x*3 + i*3;
-  fb[pixel_index + 0] = float(i) / max_x;
-  fb[pixel_index + 1] = float(j) / max_y;
-  fb[pixel_index + 2] = 0.2;
+  int pixel_index = j*max_x + i;
+  fb[pixel_index] = vec3(float(i) / max_x,
+			 float(j) / max_y,
+			 0.2);
 }
+
+/********************* MAIN ******************************************************/
 
 int main() {
 
@@ -36,10 +40,10 @@ int main() {
   std::cerr << "in " << tx << "x" << ty << " blocks.\n";
 
   int num_pixels = nx*ny;
-  size_t fb_size = 3*num_pixels*sizeof(float);
+  size_t fb_size = num_pixels*sizeof(vec3);
 
   // Allocate unified memory for frame buffer
-  float *fb;
+  vec3 *fb;
   checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
   // For timing execution of kernel code
@@ -72,14 +76,15 @@ int main() {
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      size_t pixel_index = j*3*nx + i*3;
-      float r = fb[pixel_index + 0];
-      float g = fb[pixel_index + 1];
-      float b = fb[pixel_index + 2];
-      int ir = int(255.99*r);
-      int ig = int(255.99*g);
-      int ib = int(255.99*b);
+
+      size_t pixel_index = j*nx + i;
+
+      int ir = int(255.99*fb[pixel_index].r());
+      int ig = int(255.99*fb[pixel_index].g());
+      int ib = int(255.99*fb[pixel_index].b());
+
       std::cout << ir << " " << ig << " " << ib << "\n";
+
     }
   }
 
