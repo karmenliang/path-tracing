d26c71c0e65430fd9629b9d7d6657f070c1a92c2
Karmen Liang <jl24@williams.edu>
2019-12-02 17:05:28 -0500

    pre-CUDA implementation

--- src/trace.cpp
+++ src/trace.cpp
@@ -1,3 +1,7 @@
+/*
+ * Sequential path tracer using the Monte Carlo method.
+ */
+
 #include <iostream>
 #include <float.h>
 
@@ -20,91 +24,85 @@ vec3 color(const ray& r, surface *world, int depth) {
     }
     
   }else {
-      vec3 unit_dir = unit_vector(r.direction());
-      float t = 0.5*(unit_dir.y() + 1.0);
+    vec3 unit_dir = unit_vector(r.direction());
+    float t = 0.5*(unit_dir.y() + 1.0);
     
-      return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-    }
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+  }
 }
 
 // Generate a random scene with 50 spheres
 surface *random_scene() {
-    int n = 50;
-    surface **list = new surface*[n+1];
-    list[0] =  new sphere(vec3(0,-1000,0), 1000, new matte(vec3(0.5, 0.5, 0.5)));
-    int i = 1;
+  int n = 50;
+  surface **list = new surface*[n+1];
+  list[0] =  new sphere(vec3(0,-1000,0), 1000, new matte(vec3(0.5, 0.5, 0.5)));
+  int i = 1;
 
-    for (int a = -3; a < 4; a++) {
-        for (int b = -3; b < 4; b++) {
+  for (int a = -3; a < 4; a++) {
+    for (int b = -3; b < 4; b++) {
 
-            float choose_mat = random_double();
-            vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
+      float choose_mat = random_double();
+      vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
 
-            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+      if ((center-vec3(4,0.2,0)).length() > 0.9) {
 
-                if (choose_mat < 0.5) {  // diffuse
-                    list[i++] = new sphere(center, 0.2,
-                        new matte(vec3(random_double()*random_double(),
-                                            random_double()*random_double(),
-                                            random_double()*random_double())
-                        )
-                    );
-                }
+	if (choose_mat < 0.5) {  // diffuse
+	  list[i++] = new sphere(center, 0.2,
+				 new matte(vec3(random_double()*random_double(),
+						random_double()*random_double(),
+						random_double()*random_double())
+					   )
+				 );
+	}
 
-                else { // metal
-                    list[i++] = new sphere(center, 0.2,
-                            new metal(vec3(0.5*(1 + random_double()),
-                                           0.5*(1 + random_double()),
-                                           0.5*(1 + random_double())),
-                                      0.5*random_double()));
-                }
-	        }
-        }
+	else { // metal
+	  list[i++] = new sphere(center, 0.2,
+				 new metal(vec3(0.5*(1 + random_double()),
+						0.5*(1 + random_double()),
+						0.5*(1 + random_double())),
+					   0.5*random_double()));
+	}
+      }
     }
+  }
 
-    list[i++] = new sphere(vec3(-2, 1, 0), 1.0, new matte(vec3(0.4, 0.2, 0.1)));
-    list[i++] = new sphere(vec3(2, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+  list[i++] = new sphere(vec3(-2, 1, 0), 1.0, new matte(vec3(0.4, 0.2, 0.1)));
+  list[i++] = new sphere(vec3(2, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
 
-    return new surface_list(list,i);
+  return new surface_list(list,i);
 }
 
 int main() {
-    int nx = 600;
-    int ny = 300;
-    int ns = 150; // number of samples
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  int nx = 600; // image width
+  int ny = 300; // image height
+  int ns = 150; // number of samples
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    camera cam(vec3(6,1,3), vec3(0,0,-1), vec3(0,1,0), 60, float(nx)/float(ny));
-    /*
-    surface *list[4];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new matte(vec3(0.8, 0.3, 0.3)));
-    list[1] = new sphere(vec3(0,-100.5,-1), 100, new matte(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 1.0));
-    */
-    surface *world = random_scene();
+  camera cam(vec3(6,1,3), vec3(0,0,-1), vec3(0,1,0), 60, float(nx)/float(ny));
+  surface *world = random_scene();
 
-    for (int j = ny-1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            vec3 col(0, 0, 0);
+  for (int j = ny-1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
+      vec3 col(0, 0, 0);
 
-	    // Anti-aliasing
-	    for (int s = 0; s < ns; s++) {
-                float u = float(i + random_double()) / float(nx);
-                float v = float(j + random_double()) / float(ny);
-                ray r = cam.get_ray(u, v);
-                col += color(r, world, 0);
-        }
+      // Anti-aliasing
+      for (int s = 0; s < ns; s++) {
+	float u = float(i + random_double()) / float(nx);
+	float v = float(j + random_double()) / float(ny);
+	ray r = cam.get_ray(u, v);
+	col += color(r, world, 0);
+      }
 	    
-        col /= float(ns);
-	    col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
+      col /= float(ns);
+      col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
 
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
-            
-            std::cout << ir << " " << ig << " " << ib << "\n";
-        }
+      int ir = int(255.99*col[0]);
+      int ig = int(255.99*col[1]);
+      int ib = int(255.99*col[2]);
+
+      // Writing out RGB values to ppm file
+      std::cout << ir << " " << ig << " " << ib << "\n";
     }
+  }
 
 }
